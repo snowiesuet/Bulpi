355087431e24f6ec72ebaf864db71eed19427a00
snowie <snowiesuet@gmail.com>
2021-07-16 15:17:40 -0400

    testing vue ui

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <p>{{ name }}</p>
+
+    <button @click="changeName(true)">change name</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Mikasa",
+    };
+  },
+  methods: {
+    changeName(name: number) {
+      this.name = name;
+    },
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
